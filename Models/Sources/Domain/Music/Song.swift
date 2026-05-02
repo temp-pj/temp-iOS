768e65242106e400cfe4b40c2e2a365450d93ddf
Eric <real.dassy@gmail.com>
2026-05-02 19:15:07 +0900

    [Chore] ClientMusic 컴파일을 위한 최소 모델 추가

---
+++ Models/Sources/Domain/Music/Song.swift
@@ -0,0 +1,10 @@
+//
+//  Song.swift
+//  ClientAudio
+//
+//  Created by 송지혁 on 5/2/26.
+//
+
+public struct Song {
+    public init() { }
+}
---
+++ Models/Sources/Domain/Music/SongRequest.swift
@@ -0,0 +1,10 @@
+//
+//  SongRequest.swift
+//  ClientAudio
+//
+//  Created by 송지혁 on 5/2/26.
+//
+
+public struct SongRequest: Sendable, Codable {
+    public init() { }
+}
