91ba86550dedcdf64adfc22e8aef701a4d2e01ca
Eric <real.dassy@gmail.com>
2026-05-05 15:47:49 +0900

    [Feat] GameReducer 기능 구현
    실패했던 테스트들 통과(Red -> Green)

--- Features/Game/Sources/GameReducer.swift
+++ Features/Game/Sources/GameReducer.swift
@@ -1,32 +1,78 @@
+import ClientGame
 import ComposableArchitecture
 import Foundation
 import Models
 
 @Reducer
 struct GameReducer {
+    
+    enum ServerConnectionState: String {
+        case disconnected
+        case connecting
+        case connected
+    }
+    
     @ObservableState
     struct State: Equatable {
         var roundState: RoundState = .idle
         var roundData: RoundData?
         var roundResult: RoundResult?
+        var connectionState: ServerConnectionState = .disconnected
+        var roomID: UUID
         
     }
     
     enum Action {
+        case onAppear
         case preloadSong(URL)
         case roundStart(RoundData)
         case submitAnswer(Submission)
         case answerWrong
         case roundResult(RoundResult)
+        case connectSucceed
+        case disconnected
         case penaltyFinished
         case nextRound
+        case onDisappear
     }
     
     @Dependency(\.continuousClock) var clock
+    @Dependency(\.gameClient) var gameClient
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
+                case .onAppear:
+                    let roomID = state.roomID
+                    state.connectionState = .connecting
+                    
+                    return .run { send in
+                        try await gameClient.connect(roomID)
+                        await send(.connectSucceed)
+                        
+                        for await event in gameClient.gameEvents() {
+                            switch event {
+                                case .preloadSong(let url):
+                                    await send(.preloadSong(url))
+                                    
+                                case .roundStart(let data):
+                                    await send(.roundStart(data))
+                                    
+                                case .roundResult(let result):
+                                    await send(.roundResult(result))
+                                    
+                                case .answerWrong:
+                                    await send(.answerWrong)
+                            }
+                        }
+                        
+                        await send(.disconnected)
+                    }
+                    
+                case .connectSucceed:
+                    state.connectionState = .connected
+                    return .none
+                    
                 case .preloadSong(let url):
                     state.roundState = .loading(url)
                     return .none
@@ -39,7 +85,10 @@ struct GameReducer {
                 case .submitAnswer(let submission):
                     state.roundState = .playing(.submitting)
                     
-                    return .none
+                    return .run { send in
+                        try await gameClient.submitAnswer(submission)
+                        
+                    }
                     
                 case .answerWrong:
                     state.roundState = .playing(.penalized)
@@ -69,7 +118,14 @@ struct GameReducer {
                     
                     return .none
                     
-                default: return .none
+                case .disconnected:
+                    state.connectionState = .disconnected
+                    return .none
+                    
+                case .onDisappear:
+                    return .run { send in
+                        try await gameClient.disconnect()
+                    }
             }
         }
     }
