225cc435bfee6427fc5ed31a967862a120c1ec62
Eric <real.dassy@gmail.com>
2026-05-16 17:19:12 +0900

    [Chore] FeatureGame, ClientGame 이름 변경(FeatureRoomSession, ClientRoomSession)

---
+++ Clients/RoomSessionClient/Live/Sources/RoomSessionClient+Live.swift
@@ -0,0 +1,51 @@
+//
+//  WebSocketClient+Live.swift
+//  ClientGameLive
+//
+//  Created by 송지혁 on 5/2/26.
+//
+
+import ClientRoomSession
+import ClientWebSocket
+import Dependencies
+import Foundation
+import Models
+
+enum RoomSessionError: Error {
+    case invalidURL
+    case encodingFailed
+}
+
+public extension RoomSessionClient {
+    static var live: RoomSessionClient {
+        @Dependency(\.webSocketClient) var webSocketClient
+        
+        return RoomSessionClient { roomID in
+            guard let url = URL(string: "wss://\(roomID)") else { throw RoomSessionError.invalidURL  }
+            try await webSocketClient.connect(url)
+        } send: { request in
+            let message = WebSocketMessage.from(request)
+            let data = try JSONEncoder().encode(message)
+            guard let jsonString = String(data: data, encoding: .utf8) else { throw RoomSessionError.encodingFailed }
+            try await webSocketClient.send(jsonString)
+            
+        } roomEvents: {
+            AsyncStream<RoomEvent> { continuation in
+                Task {
+                    for await rawString in await webSocketClient.receive() {
+                        if let data = rawString.data(using: .utf8) {
+                            let message = try JSONDecoder().decode(WebSocketMessage.self, from: data)
+                            let event = try message.toRoomEvent()
+                            continuation.yield(event)
+                        }
+                    }
+                    
+                    continuation.finish()
+                }
+            }
+        } disconnect: {
+            try await webSocketClient.disconnect()
+        }
+    }
+}
+    
