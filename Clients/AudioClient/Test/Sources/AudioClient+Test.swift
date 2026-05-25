8b07627f6804f55028a1752e769a0644be2a5c53
Eric <real.dassy@gmail.com>
2026-05-25 18:52:59 +0900

    [Feat] AudioClient Test 구조체에 mock 생성 메서드 추가

--- Clients/AudioClient/Test/Sources/AudioClient+Test.swift
+++ Clients/AudioClient/Test/Sources/AudioClient+Test.swift
@@ -6,15 +6,16 @@
 //
 
 import ClientAudio
+import Foundation
 import Models
 
 public extension AudioClient {
-    static let mockSuccess = AudioClient { _ in
-        
-    } play: {
-        
-    } stop: {
-        
+    static func mock(preload: @Sendable @escaping (String, TimeInterval, TimeInterval) async throws -> Void,
+                     play: @Sendable @escaping () async throws -> Void,
+                     stop: @Sendable @escaping () -> Void,
+                     playbackState: @Sendable @escaping () -> AsyncStream<PlaybackState>
+    ) -> Self {
+        return Self(preload: preload, play: play, stop: stop, playbackState: playbackState)
     }
 }
 
