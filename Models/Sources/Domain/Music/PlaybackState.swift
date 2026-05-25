6760588998ec880663b22894dcfb44ff9b50f599
Eric <real.dassy@gmail.com>
2026-05-25 18:54:00 +0900

    [Feat] PlaybackState enum 추가
    AudioClient Level에서 음악 플레이 상태를 체크할 때 사용

---
+++ Models/Sources/Domain/Music/PlaybackState.swift
@@ -0,0 +1,13 @@
+//
+//  PlaybackState.swift
+//  Models
+//
+//  Created by 송지혁 on 5/23/26.
+//
+
+public enum PlaybackState: Sendable {
+    case playing
+    case paused
+    case stopped
+    case interrupted
+}
