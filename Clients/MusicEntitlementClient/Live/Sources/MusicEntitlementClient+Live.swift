100af514f2b2884e5f0d0e4af8f4a51ca72a9c1e
Eric <real.dassy@gmail.com>
2026-05-27 16:00:14 +0900

    [Feat] MusicEntitlementClient Live 구조체 구현

--- Clients/MusicEntitlementClient/Live/Sources/MusicEntitlementClient+Live.swift
+++ Clients/MusicEntitlementClient/Live/Sources/MusicEntitlementClient+Live.swift
@@ -1,17 +1,25 @@
 //
-//  MusicClient+Live.swift
+//  MusicEntitlementClient+Live.swift
 //  ClientMusicLive
 //
 //  Created by 송지혁 on 5/2/26.
 //
 
 import ClientMusicEntitlement
+import Dependencies
+import MusicKit
 
 public extension MusicEntitlementClient {
     static let live = MusicEntitlementClient { 
-        fatalError("fetch live implementation is not implemented yet")
+        return MusicAuthorization.currentStatus
+    } requestAuthorization: {
+        return await MusicAuthorization.request()
     } checkEntitlement: {
-        fatalError("fetch live implementation is not implemented yet")
+        let subscription = try? await MusicSubscription.current
+        return subscription?.canPlayCatalogContent ?? false
     }
 }
 
+extension MusicEntitlementClient: @retroactive DependencyKey {
+    public static let liveValue: MusicEntitlementClient = .live
+}
