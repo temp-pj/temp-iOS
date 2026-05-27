28264aa4c0616e255a88b2f86ebd6f39cf395874
Eric <real.dassy@gmail.com>
2026-05-27 15:59:02 +0900

    [Feat] MusicEntitlementClient Interface에 authorizationStatus 추가

--- Clients/MusicEntitlementClient/Interface/Sources/MusicEntitlementClient.swift
+++ Clients/MusicEntitlementClient/Interface/Sources/MusicEntitlementClient.swift
@@ -2,11 +2,17 @@ import Models
 import MusicKit
 
 public struct MusicEntitlementClient: Sendable {
+    public var authorizationStatus: @Sendable () -> MusicAuthorization.Status
     public var requestAuthorization: @Sendable () async -> MusicAuthorization.Status
     public var checkEntitlement: @Sendable () async -> Bool
     
-    public init(requestAuthorization: @Sendable @escaping () async -> MusicAuthorization.Status, checkEntitlement: @Sendable @escaping () async -> Bool) {
-        self.requestAuthorization = requestAuthorization
-        self.checkEntitlement = checkEntitlement
-    }
+    public init(
+        authorizationStatus: @Sendable @escaping () -> MusicAuthorization.Status,
+        requestAuthorization: @Sendable @escaping () async -> MusicAuthorization.Status,
+        checkEntitlement: @Sendable @escaping () async -> Bool) {
+            
+            self.authorizationStatus = authorizationStatus
+            self.requestAuthorization = requestAuthorization
+            self.checkEntitlement = checkEntitlement
+        }
 }
